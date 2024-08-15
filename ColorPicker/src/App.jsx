7af1034ca514b289c5c314d78fac89139dc41df6
haythem <abdelheqr@gmail.com>
2024-08-15 17:52:49 +0100

    color picker copleted

---
+++ ColorPicker/src/App.jsx
@@ -0,0 +1,15 @@
+
+import ColorPicker from './ColorPicker'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <ColorPicker/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ ColorPicker/src/ColorPicker.jsx
@@ -0,0 +1,22 @@
+import React , {useState} from "react";
+const ColorPicker = () => {
+ const [color , setColor ] = useState("#FFFFFF")
+function  handleColorChange(event){
+ setColor(event.target.value)   ; 
+}
+
+    return ( 
+         <div className="color-picker-container">
+            <h1> Color Picker </h1>
+            <div className="color-display" style={{backgroundColor : color}}>
+            <p>selected colrod {color}</p>
+
+           
+            </div>
+             <label > select a color </label>
+            <input type="color" value={color} onChange={handleColorChange}/>
+         </div>
+     );
+}
+ 
+export default ColorPicker;
